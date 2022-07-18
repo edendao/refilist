02a491ec0105fd250bc9c6bea1bb08c29c7de0ec
Cyrus of Eden <knouroozi@gmail.com>
2022-07-18 16:04:38 -0400

    Prefetch the projects page

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,6 +1,11 @@
-import { BlitzPage } from "blitz"
+import { BlitzPage, Router, Routes } from "blitz"
+import { useEffect } from "react"
 
 const Home: BlitzPage = () => {
+  useEffect(() => {
+    Router.prefetch(Routes.Projects().pathname)
+  }, [])
+
   return null
 }
 
